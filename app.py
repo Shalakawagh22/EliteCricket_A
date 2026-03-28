745995d1dbb3f5d48ba637370d9a3affb66c1197
shalakawagh22 <shalakawagh22@gmail.com>
2026-03-29 00:40:16 +0530

    Update app.py and add Flask-Mail to requirements

--- app.py
+++ app.py
@@ -4,21 +4,27 @@ from flask_mail import Mail, Message
 from bson.objectid import ObjectId
 import random, json
 from datetime import datetime
+from dotenv import load_dotenv
+import os
 
-
+load_dotenv() 
 app = Flask(__name__)
-app.secret_key = "secretkey123"
+app.secret_key = os.getenv("FLASK_SECRET_KEY") or "secretkey123"
 
 # -------- ADMIN EMAILS --------
 ADMIN_EMAILS = ["shalakawagh22@gmail.com", "waghshalaka6@gmail.com"]
 
 # -------- MAIL CONFIG --------
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'shalakawagh22@gmail.com'
-app.config['MAIL_PASSWORD'] = 'momk ufsq onty hpcz'
-app.config['MAIL_DEFAULT_SENDER'] = 'shalakawagh22@gmail.com'
+import os
+from flask_mail import Mail
+
+app.config['MAIL_SERVER'] = os.getenv("MAIL_SERVER")
+app.config['MAIL_PORT'] = int(os.getenv("MAIL_PORT", 587))
+app.config['MAIL_USE_TLS'] = os.getenv("MAIL_USE_TLS", "True") == "True"
+app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
+app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv("MAIL_USERNAME")
+
 mail = Mail(app)
 
 # -------- MONGODB --------
